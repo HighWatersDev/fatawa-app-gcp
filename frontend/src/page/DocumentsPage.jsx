5d770cbe156ddf3591114c193583ada1e3434f2e
Rinat Kirimov <37680855+HighWatersDev@users.noreply.github.com>
2023-05-02 15:38:29 +0300

    add search function

--- frontend/src/page/DocumentsPage.jsx
+++ frontend/src/page/DocumentsPage.jsx
@@ -25,6 +25,9 @@ function DocumentsPage() {
     const [author, setAuthor] = useState('');
     const [question, setQuestion] = useState('');
     const [answer, setAnswer] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
     const handleSubmit = async (event) => {
         event.preventDefault();
         const token = await auth.currentUser.getIdToken();
@@ -45,46 +48,95 @@ function DocumentsPage() {
         }
     };
 
+    const handleSearchSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const token = await auth.currentUser.getIdToken();
+            const headers = {
+                'Authorization': `Bearer ${token}`
+            };
+            const response = await axios.get(`http://localhost:8080/v1/documents/search?search=${searchQuery}`, { headers });
+            console.log(response.data);
+            setSearchResults(response.data['documents']);
+            setErrorMessage('');
+        } catch (error) {
+            setSearchResults([]);
+            setErrorMessage(error.message);
+        }
+    };
+
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Title:
-                <input
-                    type="text"
-                    value={title}
-                    onChange={(event) => setTitle(event.target.value)}
-                />
-            </label>
-            <br />
-            <label>
-                Author:
-                <input
-                    type="text"
-                    value={author}
-                    onChange={(event) => setAuthor(event.target.value)}
-                />
-            </label>
-            <br />
-            <label>
-                Question:
-                <input
-                    type="text"
-                    value={question}
-                    onChange={(event) => setQuestion(event.target.value)}
-                />
-            </label>
-            <br />
-            <label>
-                Answer:
-                <input
-                    type="text"
-                    value={answer}
-                    onChange={(event) => setAnswer(event.target.value)}
-                />
-            </label>
-            <br />
-            <button type="submit">Submit</button>
-        </form>
+        <div>
+            <h2>Submit a New Document</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Title:
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={(event) => setTitle(event.target.value)}
+                    />
+                </label>
+                <br />
+                <label>
+                    Author:
+                    <input
+                        type="text"
+                        value={author}
+                        onChange={(event) => setAuthor(event.target.value)}
+                    />
+                </label>
+                <br />
+                <label>
+                    Question:
+                    <input
+                        type="text"
+                        value={question}
+                        onChange={(event) => setQuestion(event.target.value)}
+                    />
+                </label>
+                <br />
+                <label>
+                    Answer:
+                    <input
+                        type="text"
+                        value={answer}
+                        onChange={(event) => setAnswer(event.target.value)}
+                    />
+                </label>
+                <br />
+                <button type="submit">Submit</button>
+            </form>
+            <hr />
+            <h2>Search for Documents</h2>
+            <form onSubmit={handleSearchSubmit}>
+                <label>
+                    Search Query:
+                    <input
+                        type="text"
+                        value={searchQuery}
+                        onChange={(event) => setSearchQuery(event.target.value)}
+                    />
+                </label>
+                <br />
+                <button type="submit">Search</button>
+            </form>
+            {errorMessage && <p>{errorMessage}</p>}
+            {searchResults.length === 0 ? (
+                <p>No search results</p>
+            ) : (
+                <ul>
+                    {searchResults.map((result) => (
+                        <li key={result.id}>
+                            <h3>{result.title}</h3>
+                            <p>Author: {result.author}</p>
+                            <p>Question: {result.question}</p>
+                            <p>Answer: {result.answer}</p>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
     );
 }
 
